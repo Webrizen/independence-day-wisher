a3c2abd4f75b863ebaa3df33dc4425e342797af5
WebRizen <webrizen@gmail.com>
2023-08-11 20:11:30 +0530

    updated Navbar.jsx

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter, Poppins } from 'next/font/google';
+import Navbar from '@/components/navbar';
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ 
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  display: 'swap',
+  subsets: ['latin']
+})
 
 export const metadata = {
   title: 'Independence Day Wisher Online',
@@ -11,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   )
 }
---
+++ components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from '@/styles/components.module.css';
+import Link from 'next/link';
+import Logo from '@/assets/1.jpg';
+import Image from 'next/image';
+
+export default function Navbar() {
+  return (
+    <>
+    <header className={styles.header}>
+        <div className={styles.Logo}>
+            <Image src={Logo} alt='Logo' />
+            <span>Independence Day Wisher Online</span>
+        </div>
+        <div className={styles.links}>
+            <Link href="/">Home</Link>
+            <Link href="/donate">Donate</Link>
+            <Link href="/about">About</Link>
+        </div>
+    </header> 
+    </>
+  )
+}
