71e24a7d40a132ec49fc955c0b1923c10fa09c25
WebRizen <webrizen@gmail.com>
2023-08-12 20:25:27 +0530

    sitemap added

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://independence-day-wisher.vercel.app/',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://independence-day-wisher.vercel.app/wisher',
+      lastModified: new Date(),
+    },
+  ]
+}
