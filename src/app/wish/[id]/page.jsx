8b090c7546ecb3fe4452fd21ff75a3d9dfbcddcd
WebRizen <webrizen@gmail.com>
2023-08-12 15:08:43 +0530

    bhout h bhai

---
+++ src/app/wish/[id]/page.jsx
@@ -0,0 +1,66 @@
+import Card from "@/components/Card";
+import styles from "@/styles/wish.module.css";
+
+export default function Page({ params }) {
+  return (
+    <>
+      <div className={styles.background}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <section className={styles.onboarding}>
+        <div className={styles.landing}>
+          <Card id={params.id} />
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import styles from "@/styles/wisher.module.css";
+import Image from 'next/image';
+import Swal from "sweetalert2";
+import { db } from "@/firebase/config";
+import { doc, getDoc } from "firebase/firestore";
+
+export default function Card({ id }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from Firestore based on the passed document ID
+    const fetchData = async () => {
+      try {
+        const docRef = doc(db, "Wishes", id);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setData(docSnap.data());
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Wish not found",
+            text: "The wish you're looking for does not exist.",
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching wish:", error);
+        Swal.fire({
+          icon: "error",
+          title: "Oops... Something went wrong!",
+          text: "Unable to fetch the wish data.",
+        });
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  return (
+    <>
+      <div className={styles.wisherRight}>
+        <div className={styles.preview}>
+          <Image
+            src={data?.ProfileURL || "/placeholder.svg"}
+            alt="Profile Photo"
+            width={300}
+            height={300}
+          />
+          <h2>{data?.ReceiverName || "Receiver Name"}</h2>
+          <span className={styles.cursive}>Wish You</span>
+          <p>{data?.wishMessage || "No wish message available."}</p>
+          <span className={styles.cursive}>From</span>
+          <h3>{data?.SenderName || "Your Name"}</h3>
+        </div>
+      </div>
+    </>
+  );
+}
